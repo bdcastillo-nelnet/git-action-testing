c96a019cddbe1bb7831089f6212e125c93248936
Benjamin Castillo <benjamin.castillo@nelnet.net>
2026-06-17 20:59:28 -0600

    formatted Files

--- s3.tf
+++ s3.tf
@@ -1,14 +1,14 @@
 resource "aws_s3_bucket" "david_website_bucket" {
-    bucket = "david-website-bucket"
+  bucket = "david-website-bucket"
 }
 
 resource "aws_s3_object" "index_html" {
-    bucket = aws_s3_bucket.david_website_bucket.id
-    acl   = "private"
-    
-    key          = "index.html"
-    source       = "./web/index.html"
-    content_type = "text/html; charset=utf-8"
+  bucket = aws_s3_bucket.david_website_bucket.id
+  acl    = "private"
 
-    etag = filemd5("./web/index.html")
+  key          = "index.html"
+  source       = "./web/index.html"
+  content_type = "text/html; charset=utf-8"
+
+  etag = filemd5("./web/index.html")
 }
