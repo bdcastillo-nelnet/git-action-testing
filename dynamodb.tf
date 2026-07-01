42973edafbd8c957ae14c846e5aadbca63347e22
Benjamin Castillo <benjamin.castillo@nelnet.net>
2026-07-01 11:09:32 -0600

    Create Dynamo DB

---
+++ dynamodb.tf
@@ -0,0 +1,15 @@
+resource  "aws_dynamodb_table" "user_table" {
+  name           = "user_table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "UserTable"
+    Environment = "Production"
+  }
+}
